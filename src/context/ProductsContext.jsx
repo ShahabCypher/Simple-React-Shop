8f6354c01380627a24ea8cd47842c1d10f3c967b
Shahab Cypher <shahabcypher@gmail.com>
2025-07-31 02:33:39 +0330

    added contect for fetching products

---
+++ src/context/ProductsContext.jsx
@@ -0,0 +1,28 @@
+import { createContext, useState, useEffect } from "react";
+import api from "../services/config";
+
+const ProductsContext = createContext();
+
+const ProductsProvider = ({ children }) => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        setProducts(await api.get("/products"));
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
+  return (
+    <ProductsContext.Provider value={products}>
+      {children}
+    </ProductsContext.Provider>
+  );
+};
+
+export default ProductsProvider;
