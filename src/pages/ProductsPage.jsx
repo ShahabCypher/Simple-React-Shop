0b487ea6248b60baef115ed06831ba897468dcda
Shahab Cypher <shahabcypher@gmail.com>
2025-07-31 03:55:45 +0330

    added search section

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import { ImSearch } from "react-icons/im";
+
 import { useProducts } from "../context/ProductsContext";
 import Card from "../components/Card";
 import Loader from "../components/Loader";
@@ -7,16 +10,33 @@ import styles from "./styles/ProductsPage.module.css";
 const ProductsPage = () => {
   const products = useProducts();
 
+  const [search, setSearch] = useState("");
+
+  const searchHandler = () => {};
+
   return (
-    <div className={styles.container}>
-      <div className={styles.products}>
-        {!products.length && <Loader />}
-        {products.map((p) => (
-          <Card key={p.id} data={p} />
-        ))}
+    <>
+      <div>
+        <input
+          type="text"
+          placeholder="Search..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value.toLowerCase())}
+        />
+        <button onClick={searchHandler}>
+          <ImSearch />
+        </button>
       </div>
-      <div></div>
-    </div>
+      <div className={styles.container}>
+        <div className={styles.products}>
+          {!products.length && <Loader />}
+          {products.map((p) => (
+            <Card key={p.id} data={p} />
+          ))}
+        </div>
+        <div></div>
+      </div>
+    </>
   );
 };
 
