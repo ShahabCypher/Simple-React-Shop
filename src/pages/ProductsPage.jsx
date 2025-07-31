98c9bcb190edbed4f5066b547b4b58612509a62b
Shahab Cypher <shahabcypher@gmail.com>
2025-07-31 03:42:43 +0330

    added products card component

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,9 +1,22 @@
 import { useProducts } from "../context/ProductsContext";
+import Card from "../components/Card";
+
+import styles from "./styles/ProductsPage.module.css";
 
 const ProductsPage = () => {
   const products = useProducts();
-  console.log(products);
-  return <div>ProductsPage</div>;
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.products}>
+        {!products.length && <p>Loading...</p>}
+        {products.map((p) => (
+          <Card key={p.id} data={p} />
+        ))}
+      </div>
+      <div></div>
+    </div>
+  );
 };
 
 export default ProductsPage;
