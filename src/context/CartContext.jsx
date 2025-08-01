03aa6d6652feabecdd45722fa66a75ff559a6691
Shahab Cypher <shahabcypher@gmail.com>
2025-08-02 01:43:26 +0330

    added remove item action to cart context reducer

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -20,6 +20,16 @@ const reducer = (state, action) => {
         ...sumProducts(state.selectedItems),
       };
 
+    case "REMOVE_ITEM":
+      const newSelectedItems = state.selectedItems.filter(
+        (item) => item.id !== action.payload.id
+      );
+      return {
+        ...state,
+        selectedItems: [...newSelectedItems],
+        ...sumProducts(newSelectedItems),
+      };
+
     default:
       throw new Error("Invalid action");
   }
